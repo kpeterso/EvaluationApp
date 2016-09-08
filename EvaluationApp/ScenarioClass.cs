a4b2e340c0cef22b37c9717b45aa1c391a2739cd
kpete <kpete@DESKTOP-U9I9TBV>
2016-09-08 12:47:36 -0700

    Add project files.

---
+++ EvaluationApp/ScenarioClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationApp
+{
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public static async Task<Windows.Data.Xml.Dom.XmlDocument> LoadXmlFile(String folder, String file)
+        {
+            Windows.Storage.StorageFolder storageFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(folder);
+            Windows.Storage.StorageFile storageFile = await storageFolder.GetFileAsync(file);
+            Windows.Data.Xml.Dom.XmlLoadSettings loadSettings = new Windows.Data.Xml.Dom.XmlLoadSettings();
+            loadSettings.ProhibitDtd = false;
+            loadSettings.ResolveExternals = false;
+            return await Windows.Data.Xml.Dom.XmlDocument.LoadFromFileAsync(storageFile, loadSettings);
+        }
+    }
+}
