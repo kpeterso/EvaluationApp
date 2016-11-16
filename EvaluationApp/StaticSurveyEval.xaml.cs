de22f0268608ebfefb4791b650d898a13f1158a6
kpeterso <kpeterso@hotmail.com>
2016-11-16 12:03:56 -0700

    Removing evaluation class from staticsurveyeval

--- EvaluationApp/StaticSurveyEval.xaml.cs
+++ EvaluationApp/StaticSurveyEval.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,8 +24,7 @@ namespace EvaluationApp
     {
         private MainPage rootPage;
 
-        private string ID;
-        private Evaluation evaluation;
+        private SpeechRecognizer speechRecognizer;
         private ObservableCollection<Observation> oList;
         private string surveyName;
 
@@ -33,22 +33,25 @@ namespace EvaluationApp
         public StaticSurveyEval()
         {
             this.InitializeComponent();
+            oList = new ObservableCollection<Observation>();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             rootPage = MainPage.Current;
-            if (e.Parameter is string)
-            {
-                ID = e.Parameter.ToString();
 
-                loadData();
-                initSurvey();
-            }
+            initSurvey();
+
+            //dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+            speechRecognizer = new SpeechRecognizer();
+
+            //Compile the dictation grammar by default
+            await speechRecognizer.CompileConstraintsAsync();
         }
 
         private void initSurvey()
         {
+            surveyName = "Test Survey";
             Survey survey = Survey.findSurvey(surveyName);
             questionList = new ObservableCollection<surveyQuestion>(survey.surveyQuestionList);
         }
@@ -57,22 +60,42 @@ namespace EvaluationApp
         private void listBox_surveyQuestions_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selectedResponse = evaluation.surveyResponseList.ElementAt(listBox_surveyQuestions.SelectedIndex);
-            textBlock_commentText.Text = selectedResponse.comment;
-            textBlock_ratingText.Text = selectedResponse.rating.ToString();
+            textBox_comment.Text = selectedResponse.comment;
+            //textBlock_ratingText.Text = selectedResponse.rating.ToString();
         }
 
-        private void loadData()
+        private void SubmitObservationButton_Click(object sender, RoutedEventArgs e)
         {
-            foreach (Evaluation eval in Evaluation.evaluationList)
-            {
-                if (eval.evalID == ID.ToString())
-                {
-                    evaluation = new Evaluation(eval);
-                    break;
-                }
-            }
-            oList = new ObservableCollection<Observation>(evaluation.observationList);
-            surveyName = evaluation.surveyName;
+            var comment = textBox_comment.Text;
+            string timestamp = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.ff");
+            Observation obs = new EvaluationApp.Observation() { comment = comment, timestamp = timestamp };
+            oList.Add(obs);
+            textBox_comment.Text = "";
+        }
+
+        private void button_EndEval_Click(object sender, RoutedEventArgs e)
+        {
+            string driver = textBox_name.Text;
+            string vehicle = textBlock_MakeText.Text + " " + textBlock_ModelText.Text;
+            string type = "Static Evaluation";
+            string ts = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.ff");
+
+            evaluation = new Evaluation(driver, vehicle, type, null, ts, oList);
+            Evaluation.evaluationList.Add(evaluation);
+
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.GoBack();
+        }
+
+        private async void StartVoiceRecognition_Click(object sender, RoutedEventArgs e)
+        {
+            string textBoxText = textBox_comment.Text;
+
+            //Start Recognition
+            Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeWithUIAsync();
+
+            //Put recognition result in commentTextBlock
+            textBox_comment.Text = textBoxText + " " + speechRecognitionResult.Text;
         }
     }
 }
